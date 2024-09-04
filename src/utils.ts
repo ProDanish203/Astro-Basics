6e912e85afd257056c0f3fbb78d9be8e97aaa642
Danish <danishsidd524@gmail.com>
2024-09-05 04:35:32 +0500

    add: inital setup, page routing, image component, components and props, and data fetching

---
+++ src/utils.ts
@@ -0,0 +1,14 @@
+export const formatDate = (date: string) => {
+    const options: Intl.DateTimeFormatOptions = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    return new Date(date).toLocaleDateString("en-US", options);
+};
+
+export const capitalize= (str: string): string =>  {
+    if (typeof str !== 'string' || str.length === 0) 
+      return str;
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
